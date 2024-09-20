cf2974d16d5a61de19fcc20005bd415a179a2764
sandsocks <max.sandejas@gmail.com>
2024-09-21 01:05:04 +0800

    AYAW PARIN GUMANA WTF

--- Assets/NuhUh.cs
+++ Assets/NuhUh.cs
@@ -5,42 +5,26 @@ using UnityEngine;
 
 public class NuhUh : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject target;
-    public GameObject test;
+    public GameObject Player, Target, Test; 
+    public Vector2 direction, normalized, pointInBetween;
+    public float distance;
+    private float playerPosition, targetPosition, testPosition;
 
-    public Vector2 direction, normalized;
-
-    public float distance, PlayerTestDistance, TargetTestDistance, D;
-
+    public bool isInBetween;
+    
     void Update()
     {
-        Vector2 playerPosition = player.transform.position;
-        Vector2 targetPosition = target.transform.position;
-        Vector2 testPosition = test.transform.position;
+       direction = Test.transform.position - Player.transform.position;
 
-        direction = target.transform.position - player.transform.position;
-        distance = Mathf.Sqrt((playerPosition.x * targetPosition.x) + (playerPosition.y * targetPosition.y));
+        Vector2 playerPosition = Player.transform.position;
+        Vector2 targetPosition = Target.transform.position;
+        Vector2 testPosition = transform.position;
+
+        distance = Mathf.Sqrt((direction.x * direction.x) + (direction.y * direction.y));
         normalized = direction / distance;
 
-        playerPosition = player.transform.position;
-        targetPosition = target.transform.position;
-        testPosition = test.transform.position;
-        
-        PlayerTestDistance = Mathf.Sqrt((playerPosition.x * testPosition.x) + (playerPosition.y * testPosition.y));
-        TargetTestDistance = Mathf.Sqrt((targetPosition.x * testPosition.x) + (targetPosition.y * testPosition.y));
-
-        D = PlayerTestDistance + TargetTestDistance;
-        
-        if (D == distance)
-        {
-            Debug.Log("Is in between");
-        }
-
-        if (D != distance)
-        {
-            Debug.Log("Is not in between");
-        }
+        isInBetween = IsPointInBetween (playerPosition, targetPosition, testPosition);
     }
-
 }
+
+
--- Assets/VectorsTest.cs
+++ Assets/VectorsTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class VectorsTest : MonoBehaviour
-{
+/*{
     public GameObject Player, Target, Test; 
     public Vector2 direction, normalized, pointInBetween;
     public float distance;
@@ -22,5 +22,56 @@ public class VectorsTest : MonoBehaviour
         distance = Mathf.Sqrt((direction.x * direction.x) + (direction.y * direction.y));
         normalized = direction / distance;
 
+
+
+    }
+}*/
+{
+public GameObject Player, Target;
+    public float PlayerTestMagnitude, PlayerTargetMagnitude;
+
+    void Start()
+    {
+        Vector2 playerPosition = Player.transform.position;
+        Vector2 targetPosition = Target.transform.position;
+        Vector2 testPosition = transform.position;
+
+        bool isInBetween = IsPointInBetween (playerPosition, targetPosition, testPosition);
+
+        if (isInBetween)
+        {
+            Debug.Log("Is in between");
+        }
+
+        else
+        {
+            Debug.Log("Is not in between");
+        }
+    }
+
+    bool IsPointInBetween (Vector2 player, Vector2 target, Vector2 test)
+    {
+        Vector2 PlayerTargetDirection = target - player;
+        Vector2 PlayerTestDistance = test - player;
+
+        float dotProduct = Vector2.Dot(PlayerTestDistance, PlayerTargetDirection);
+
+        if (dotProduct > 0)
+        {
+            PlayerTargetMagnitude = Mathf.Sqrt((PlayerTargetDirection.x * PlayerTargetDirection.x) + (PlayerTargetDirection.y * PlayerTargetDirection.y));
+            PlayerTestMagnitude = Mathf.Sqrt ((PlayerTestDistance.x * PlayerTestDistance.x) + (PlayerTestDistance. y * PlayerTestDistance.y));
+
+            if (PlayerTestMagnitude <= PlayerTargetMagnitude)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+        
     }
 }
+
+
+
+
